1b1e97ff7930f8a16a94e2a39216c160d6921cf6
angeltorx <angelfitorres@gmail.com>
2023-03-07 22:37:30 -0500

    5
    StartJS

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const saveBtn = document.getElementById("saveBtn");
+    const changeThemeBtn = document.getElementById("changeThemeBtn");
+    const inputIdentification = document.getElementById("inputIdentification");
+    const inputName = document.getElementById("inputName");
+    const inputDireccion = document.getElementById("inputDireccion");
+    const inputEstrato = document.getElementById("inputEstrato");
+    const inputEmail = document.getElementById("inputEmail");
+    let tableBody = document.getElementById("tableBody")
+
+
+
+
+});
